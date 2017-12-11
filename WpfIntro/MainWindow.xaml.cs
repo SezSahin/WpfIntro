69cfcfdc11eef9f07ad5366ff3ddd7ad5baae259
SezSahin <sezg0006@edu.eal.dk>
2017-12-11 09:12:26 +0100

    Add project files.

---
+++ WpfIntro/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIntro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrolUp_Click(object sender, RoutedEventArgs e)
+        {
+            string lines = Line1.Text.ToString();
+            Line1.Text = Line2.Text;
+            Line2.Text = Line3.Text;
+            Line3.Text = Line4.Text;
+            Line4.Text = lines;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Line1.Text = "";
+            Line2.Text = "";
+            Line3.Text = "";
+            Line4.Text = "";
+        }
+
+        private void ScrolDown_Click(object sender, RoutedEventArgs e)
+        {
+            string lines = Line1.Text.ToString();
+            Line1.Text = Line4.Text;
+            Line4.Text = Line3.Text;
+            Line3.Text = Line2.Text;
+            Line2.Text = lines;
+        }
+    }
+}
